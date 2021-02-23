ff2ab4b0f2d8e0335829f587ac1e70c49ac71e82
Randy rebucas <rebucasrandy1986@gmail.com>
2021-02-23 20:18:40 +0800

    update

--- src/app/pages/offers/detail/detail.component.ts
+++ src/app/pages/offers/detail/detail.component.ts
@@ -1,8 +1,12 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { IonRouterOutlet, ModalController, NavParams } from '@ionic/angular';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage';
+import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/camera/ngx';
+import { ActionSheetController, AlertController, LoadingController, ModalController, NavParams } from '@ionic/angular';
 import { from, Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { finalize, switchMap } from 'rxjs/operators';
 import { AuthService } from 'src/app/auth/auth.service';
+import { Base64 } from 'src/app/helper/base64';
+import { FirestoreService } from 'src/app/shared/services/firestore.service';
 import { SubSink } from 'subsink';
 import { SettingsService } from '../../settings/settings.service';
 import { FormComponent } from '../form/form.component';
@@ -14,24 +18,34 @@ import { OffersService } from '../offers.service';
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss'],
 })
-export class DetailComponent implements OnInit {
+export class DetailComponent implements OnInit, OnDestroy {
   public offer$: Observable<any>;
+  public uploadPercent: Observable<number>;
+  public offer: Offers;
   public title: string;
+  public showProgress: boolean;
   public defaultCurrency: string;
+  private angularFireUploadTask: AngularFireUploadTask;
+  private angularFireStorageReference: AngularFireStorageReference;
   private subs = new SubSink();
 
   constructor(
     private navParams: NavParams,
     private modalController: ModalController,
+    private alertController: AlertController,
+    private loadingController: LoadingController,
+    private actionSheetController: ActionSheetController,
     private authService: AuthService,
     private settingsService: SettingsService,
     private offersService: OffersService,
+    private firestoreService: FirestoreService,
+    private camera: Camera,
   ) { }
 
   ngOnInit() {
     this.title = this.navParams.data.title;
 
-    from(this.authService.getCurrentUser()).pipe(
+    this.subs.sink = from(this.authService.getCurrentUser()).pipe(
       switchMap((user) => {
         return this.settingsService.getOne(user.uid);
       })
@@ -45,8 +59,8 @@ export class DetailComponent implements OnInit {
       })
     );
 
-    this.offer$.subscribe((r) => {
-      console.log(r);
+    this.subs.sink =  this.offer$.subscribe((offer) => {
+      this.offer = offer;
     });
   }
 
@@ -68,5 +82,112 @@ export class DetailComponent implements OnInit {
     this.modalController.dismiss({
       dismissed: state
     });
+  }
+
+  onPickSource() {
+    this.subs.sink = from(this.actionSheetController.create({
+      header: 'Pick a Source',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Library',
+        icon: 'library-outline',
+        handler: () => {
+          this.takePhoto(this.camera.PictureSourceType.PHOTOLIBRARY);
+        }
+      }, {
+        text: 'Camera',
+        icon: 'camera-outline',
+        handler: () => {
+          this.takePhoto(this.camera.PictureSourceType.CAMERA);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {}
+      }]
+    })).subscribe((actionSheetEl) => {
+      actionSheetEl.present();
+    });
+  }
+
+  takePhoto(selectedSourceType: PictureSourceType) {
+    this.capture(selectedSourceType);
+  }
+
+  capture(selectedSourceType: PictureSourceType) {
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: selectedSourceType,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      cameraDirection: this.camera.Direction.FRONT
+    };
+
+    this.subs.sink = from(this.camera.getPicture(options)).subscribe((imageData) => {
+      this.onUpload(imageData);
+    });
+  }
+
+  onUpload(imageData: any) {
+    this.subs.sink = from(this.loadingController.create({
+      message: 'Please wait...'
+    })).subscribe(loadingEl => {
+      loadingEl.present();
+      this.doUpload(imageData);
+    });
+  }
+
+  doUpload(imageUrl: any) {
+    this.showProgress = true;
+    const file = new Base64().dataURItoBlob('data:image/jpeg;base64,' + imageUrl);
+    const filePath = `offer/${this.offer.id}.jpg`;
+
+    this.angularFireStorageReference = this.firestoreService.ref(filePath);
+    this.angularFireUploadTask = this.firestoreService.put(filePath, file);
+    // observe percentage changes
+    this.uploadPercent = this.angularFireUploadTask.percentageChanges();
+
+    this.subs.sink = this.angularFireUploadTask.snapshotChanges().pipe(
+      finalize(() => {
+        this.subs.sink = this.angularFireStorageReference.getDownloadURL().subscribe((imageLink) => {
+          this.getCurrentUser(imageLink);
+        });
+      })
+    ).subscribe();
+  }
+
+  getCurrentUser(imageLink: any) {
+    this.subs.sink = from(this.authService.getCurrentUser()).subscribe((currentUser) => {
+      this.setUpdateData(imageLink, currentUser.uid);
+    }, (error: any) => {
+      this.loadingController.dismiss();
+      this.presentAlert(error.code, error.message);
+    });
+  }
+
+  setUpdateData(imageLink: any, userId: string) {
+    this.subs.sink = from(this.offersService.update(userId, this.offer.id, {imageUrl: imageLink})).subscribe(() => {
+      this.loadingController.dismiss();
+      this.onDismiss(true);
+    }, (error: any) => {
+      this.loadingController.dismiss();
+      this.presentAlert(error.code, error.message);
+    });
+  }
+
+  presentAlert(alertHeader: string, alertMessage: string) {
+    this.subs.sink = from(this.alertController.create({
+      header: alertHeader, // alert.code,
+      message: alertMessage, // alert.message,
+      buttons: ['OK']
+    })).subscribe(alertEl => {
+        alertEl.present();
+    });
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 }
